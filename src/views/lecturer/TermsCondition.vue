7f2231816eff98cd95a029a2bb11425ee97b546d
oolayiwol <sirmuel360@gmail.com>
2024-10-04 17:38:07 +0100

    make it better

--- src/views/lecturer/TermsCondition.vue
+++ src/views/lecturer/TermsCondition.vue
@@ -34,27 +34,27 @@
           <v-col cols="12" sm="12">
             <h1 class="mt-5">Terms and Conditions</h1>
             <h4 class="font-weight-medium">
-              Ensure to read throught the terms and conditions before you
+              Ensure to read through the terms and conditions before you
               proceed
             </h4>
 
             <v-card class="pa-5 mt-2 blue--text text--darken-4" outlined>
               <h3>Terms and Conditions for Track-CA</h3>
-              <h4 class="mt-2">
+              <h4 class="mt-2 font-weight-regular">
                 1. Introduction These Terms and Conditions govern your use of
                 our School Management Application (EduTrack). By accessing or
                 using the Application, you agree to be bound by these terms. If
                 you do not agree to these terms, please refrain from using the
                 Application.
             </h4>
-            <h4 class="mt-2">
+            <h4 class="mt-2 font-weight-regular">
                 2. License Grant We grant you a non-exclusive,
                 non-transferable license to use the Application for the sole
                 purpose of managing your school's operations. You may not
                 modify, distribute, or create derivative works of the
                 Application.
             </h4> 
-            <h4 class="mt-2">
+            <h4 class="mt-2 font-weight-regular">
                 3. User Data You are responsible for the accuracy
                 and completeness of the data you enter into the Application. We
                 may collect and store information about your use of the
@@ -62,19 +62,19 @@
                 information is used to improve the Application and provide
                 better service. 
             </h4>
-            <h4 class="mt-2">
+            <h4 class="mt-2 font-weight-regular">
                 4. Intellectual Property The Application and its
                 contents, including but not limited to the software, design, and
                 trademarks, are the property of [Your Company Name] and are
                 protected by copyright and other intellectual property laws. 
             </h4>
-            <h4 class="mt-2">
+            <h4 class="mt-2 font-weight-regular">
                 5.
                 Privacy Policy Your privacy is important to us. Please refer to
                 our Privacy Policy for details on how we collect, use, and
                 protect your personal information.
             </h4>    
-            <h4 class="mt-2">
+            <h4 class="mt-2 font-weight-regular">
                 6. Disclaimer of Warranties
                 The Application is provided "as is" without any warranties,
                 express or implied. We do not guarantee the accuracy,
